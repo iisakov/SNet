60b94f7e6146a69b9119f0b3f156ea9dd3cdd51f
iisakov <59264679+iisakov@users.noreply.github.com>
2022-06-05 19:55:22 +0300

    Update connection.py

--- config/connect_vk/connection.py
+++ config/connect_vk/connection.py
@@ -1,5 +1,5 @@
 import requests as r
 
-access_token = '0cd7052fdfb8771d1c95661102846c07a31506409157a2c384fabedf2018e578daf9efde0b8681f51acc6'
+access_token = ''
 method_url = f'https://api.vk.com/method/groups.getMembers?v=5.131&group_id=by_artisan&access_token={access_token}'
 connect = r.get(url=method_url)
